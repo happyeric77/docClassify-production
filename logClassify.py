421ece0cdf069e94edf0f871c3bcdd04f4040a69
Eric Lee <macbookpro4eric@Erics-MacBook-Pro.local>
2020-07-29 18:04:25 +0900

    Add function test report

--- logClassify.py
+++ logClassify.py
@@ -16,12 +16,10 @@ if not os.path.exists('98B-500-0010R-LTE_1B.G-99B-501-0006R'):
     print('create LTE Grey folder ')
     os.mkdir('98B-500-0010R-LTE_1B.G-99B-501-0006R')
 
-
 # If no WIFI Black folder, create one
 if not os.path.exists('98B-500-0011R-WIFI_1A.B-99B-501-0005R'):
     print('create WIFI Black dir')
     os.mkdir('98B-500-0011R-WIFI_1A.B-99B-501-0005R')
-
 
 # If no WIFI Grey folder, create one
 if not os.path.exists('98B-500-0012R-WIFI_1A.G-99B-500-0005R'):
@@ -54,11 +52,10 @@ for snTable in snTables:
 
 products = [lteBlack, lteGrey, wifiBlack, wifiGrey]
 
-
 for product in products:
     print('\n' * 3 + '-' * 50 + '\n{product} section start\n'.format(product=product['name']) + '-' * 50 + '\n' * 3)
-    allFiles= os.listdir(product['path'])
-    # seperate function test files and final test files to different array
+    allFiles = os.listdir(product['path'])
+    # Seperate function test files and final test files to different array
     funcLogNames = []
     logNames = []
 
@@ -77,11 +74,9 @@ for product in products:
     print('\nfinal test log files:')
     print(logNames)
 
-    #Final test data fields, and generate final test report
+    # Final test data fields, and generate final test report
     print('*' * 50 + '\nStart generating "{product}" final test rerport. \n'.format(product=product['name']) + '*' * 50)
 
-
-    # LTEinfo = {}
     data = {
         'Application Version': [],
         'Test Time': [],
@@ -199,28 +194,132 @@ for product in products:
         os.mkdir(targetDir)
     pd.DataFrame.from_dict(data, orient='index').T.to_excel(os.path.join(targetDir, fileName))
 
-    # #Function test data fields, and generate function test report
+    # Function test data fields, and generate function test report
     print('*'*50 + '\nstart generating {product} function test rerport\n'.format(product=product['name']) + '*'*50)
 
     funcData = {
         'Application Version': [],
-        'Test Time': [],
-        'DUT SN': [],
-        'DUT MAC': [],
         'Detect device and comport': [],
-        'BLE firmware': [],
-        'FW Version': [],
-        'FW Final Check': [],
-        'Check LTE Information': [],
-        'WIFI function': [],
+        'Burn BLE': [],
+        'SOC version': [],
+        'BLE version': [],
+        'Test Check Firmware': [],
+        'Test LTE Connection': [],
+        'Test WIFI connection': [],
+        'Test BLE': [],
+        'Test LED': [],
+        'Test Button': [],
+        'Format': [],
     }
 
     try:
         for funcLogName in funcLogNames:
-            print(funcLogName)
-    except:
+
+            if not re.findall(r"\.", funcLogName):
+                funcLogName=funcLogName + '.'
+
+            preFix, proFix = funcLogName.split(".")
+
+            def get_mask_value(condition):
+                mask = funcLog[0].str.startswith(condition)
+                return funcLog[mask].values[0]
+
+            def check_function(logContent, contentToFill):
+                try:
+                    duc = get_mask_value(logContent)[0]
+                    if duc and duc != '':
+                        funcData[contentToFill] += ['PASS']
+                    else:
+                        if ((len(re.findall(r'^WIFI', product['name'])) > 0) and (contentToFill == 'Test LTE Connection')) or ((len(re.findall(r'^LTE', product['name'])) > 0) and (contentToFill == 'Test WIFI connection')):
+                            funcData[contentToFill] += ['N/A']
+                        else:
+                            funcData[contentToFill] += ['FAIL']
+                except:
+                    if ((len(re.findall(r'^WIFI', product['name'])) > 0) and (contentToFill == 'Test LTE Connection')) or ((len(re.findall(r'^LTE', product['name'])) > 0) and (contentToFill == 'Test WIFI connection')):
+                        funcData[contentToFill] += ['N/A']
+                    else:
+                        funcData[contentToFill] += ['FAIL']
+
+            try:
+                funcLog = pd.read_csv(product['path']+funcLogName, header=None)
+                # Retrieve Application version
+                try:
+                    detail = get_mask_value('Application Version:')[0]
+                    _, version = detail.split(':')
+                    value = version.replace(' ', '')
+                    funcData['Application Version'] += [value]
+                except:
+                    funcData['Application Version'] += ['Unknown']
+
+                # Retrieve Detected device comport
+                try:
+                    detail = get_mask_value('Detected device comport=COM')
+                    if len(detail[0])>0:
+                        funcData['Detect device and comport'] += ['PASS']
+                    else:
+                        funcData['Detect device and comport'] += ['FAIL']
+                except:
+                    funcData['Detect device and comport'] += ['FAIL']
+
+                # Check BLE burn
+                check_function('[Burn BLE]', 'Burn BLE')
+
+                # Retrieve SOC version and check if it matches production version
+                try:
+                    detail = get_mask_value('SOC version')[0]
+                    vut, productionVersion = re.findall(r"v\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2}", detail)
+                    if vut == productionVersion:
+                        funcData['SOC version'] += [vut]
+                    else:
+                        funcData['SOC version'] += ['FAIL']
+                except:
+                    funcData['SOC version'] += ['FAIL']
+
+                # Retrieve BLE version and check if it matches production version
+                try:
+                    detail = get_mask_value('BLE version')[0]
+                    vut, productionVersion = re.findall(r"v\d{1,2}.\d{1,2}.\d{1,2}.\d{1,2}", detail)
+                    if vut == productionVersion:
+                        funcData['BLE version'] += [vut]
+                    else:
+                        funcData['BLE version'] += ['FAIL']
+                except:
+                    funcData['BLE version'] += ['FAIL']
+
+                # Test check firmware
+                check_function('Test Check Firmware PASS', 'Test Check Firmware')
+
+                # Test LTE connection
+                check_function('Test LTE Connection: PASS', 'Test LTE Connection')
+
+                # 'Test WIFI connection'
+                check_function('Test WiFi Connection: PASS', 'Test WIFI connection')
+
+                # Test BLE
+                check_function('Test BLE Echo: PASS', 'Test BLE')
+
+                # 'Test LED'
+                check_function('TEST LED PASS', 'Test LED')
+
+                # 'Test Button'
+                check_function('Test Button: PASS', 'Test Button')
+
+                # 'Format'
+                check_function('formating...', 'Format')
+
+                print('file "{product}" succeeded to generate "Function test" report.'.format(product=file))
+
+            except Exception as e:
+                print(funcLogName + ' fail: Read file fail')
+                print(e)
+
+    except Exception as e:
         print('file "{product}" fail to generate "function test" report.'.format(product=file))
 
-
-
     print('\n' * 3 + '-' * 50 + '\n{product} section done\n'.format(product=product['name'])+ '-' * 50 + '\n' * 10)
+
+    targetDir = "functionTestReport"
+    fileName = "functionTestReport_{product}_{date}.xlsx".format(date=datetime.datetime.now().strftime('%Y%m%d%H'), product=product['name'])
+    if not os.path.exists(targetDir):
+        os.mkdir(targetDir)
+    pd.DataFrame.from_dict(funcData, orient='index').T.to_excel(os.path.join(targetDir, fileName))
